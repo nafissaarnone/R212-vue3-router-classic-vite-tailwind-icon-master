f0435fd11e8a327fadb4675195c8378f6ad287d9
nafissaarnone <95024629+nafissaarnone@users.noreply.github.com>
2022-05-16 10:04:43 +0200

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      "fontFamily": {
+        "inter": "Inter, system-ui, sans-serif"
+      }
+    }
+  },
+
+}
